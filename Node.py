3d8fbc8247afe49fff83b0a93eb6bca79bf9e9c4
Victor Arreaga <arreagav17@gmail.com>
2022-02-25 12:28:10 -0500

    refactor find node

--- Node.py
+++ Node.py
@@ -24,7 +24,7 @@ class Node:
         if not current_children:
             self.__childrens[direction] = children
             children.__parent = self
-            if self.__childrens.__len__(2):
+            if self.__childrens.__len__() == 2:
                 self.__type_node = TypesNode.PARENT_WITH_TWO_CHILDS
             else:
                 self.__type_node = TypesNode.PARENT_WITH_ONE_CHILD
@@ -33,41 +33,55 @@ class Node:
             current_children.add(children)
 
     def get(self, direction: Direction):
-        return self.__childrens[direction._value_]
+        return self.__childrens.get(direction)
+
+    def __get_last_in(self, direction: Direction):
+        if not self.__type_node.is_leaf():
+            iterator = self.get(direction)
+            while iterator:
+                iterator = iterator.get(direction)
+            return iterator
+
+        return self
 
     def delete(self, delete_node) -> None:
+        print("eliminar ", delete_node)
         if delete_node.__type_node.is_parent_with_two_childs():
-            change_node = delete_node.get(Direction.RIGHT).__get_last_in(Direction.LEFT)
-            change_node = delete_node
-            delete_parent_node = delete_node.__parent
-            for direction, node in delete_parent_node.__childrens.items():
-                if node == delete_node:
-                    delete_parent_node.__childrens[direction] = change_node
+            print(self.__get_last_in(Direction.LEFT))
+            # change_node = delete_node.get(Direction.RIGHT).__get_last_in(Direction.LEFT)
+            # change_node = delete_node
+            # delete_parent_node = delete_node.__parent
+            # for direction, node in delete_parent_node.__childrens.items():
+            #     if node == delete_node:
+            #         delete_parent_node.__childrens[direction] = change_node
 
         elif delete_node.__type_node.is_parent_with_one_child():
-            right, left = delete_node.get(Direction.RIGHT), delete_node.get(
-                Direction.LEFT
-            )
-            if right:
-                right.changes_position(delete_node)
-            elif left:
-                left.changes_position(delete_node)
+            pass
+            # right, left = delete_node.get(Direction.RIGHT), delete_node.get(
+            #     Direction.LEFT
+            # )
+            # if right:
+            #     right.changes_position(delete_node)
+            # elif left:
+            #     left.changes_position(delete_node)
         else:
-            delete_node.__parent = None
+            pass
+            # delete_node.__parent = None
 
     def changes_position(self, parent) -> None:
         self.__parent = parent.__parent
         parent.__parent = None
 
-    def exists(self, node) -> bool:
-        if node:
+    def find(self, node) -> bool:
+        find = self == node
+        if not find:
             if node > self:
                 check = self.get(Direction.RIGHT)
             else:
                 check = self.get(Direction.LEFT)
             if check:
-                return check.exists(node)
-        return self == node
+                return check.find(node)
+        return find
 
     def get_childrens(self) -> list:
         return self.__childrens
